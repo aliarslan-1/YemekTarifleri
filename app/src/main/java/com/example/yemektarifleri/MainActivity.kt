3cb049e8de7c8c18c313f96e34a8eefc1d2b8ae0
Ali Arslan <64864432+aliarslan-1@users.noreply.github.com>
2023-02-05 21:59:01 +0300

    item
    item

--- app/src/main/java/com/example/yemektarifleri/MainActivity.kt
+++ app/src/main/java/com/example/yemektarifleri/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.yemektarifleri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.yemek_ekle,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.yemek_ekleme_item){
+            
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
